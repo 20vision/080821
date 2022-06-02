d21e574c322684d83345f3f61db9e26d2914e310
Alex <business@20.vision>
2022-06-02 18:51:02 +0200

    minor fix

--- browser/pages/saved.jsx
+++ browser/pages/saved.jsx
@@ -1,13 +1,11 @@
 import dynamic from 'next/dynamic'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import PaperPreview from '../components/User/Carousel/PaperPreview'
 const DefaultLayout = dynamic(
   () => import("../layouts/default"),
   {ssr : false}
 )
 import useUserProfile from '../hooks/User/useUserProfile'
-import { toast } from 'react-toastify'
-import { useRouter } from 'next/router'
 
 export default function Home() {
   const [selectedComponent, setSelectedComponent] = useState()
